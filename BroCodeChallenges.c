63c02388dc404e8d73229ebf99588f3464b2394e
Adam Davoren King <adamdavorenking21@gmail.com>
2024-12-28 18:15:02 +0000

    hypotenuse calculator program

--- BroCodeChallenges.c
+++ BroCodeChallenges.c
@@ -1,19 +1,37 @@
 /*Write a program to calculate the circumference of a circle*/
 #include <stdio.h>
-int main() {
+//int main() {
+//
+//	const double PI = 3.14159; //sets PI so that it cannot be altered later
+//	double radius, circumference, area;
+//
+//	printf("\nPLEASE ENTENPUT RADIUS\n");
+//	scanf_s("%lf", &radius);
+//
+//	circumference = 2 * PI * radius;
+//	printf("the cicumference of the cirlce of that radius is %lf\n", circumference);
+//
+//	area = PI * radius * radius;
+//	printf("the area of the cirlce of that radius is %lf\n", area);
+//
+//
+//	return 0;
+//}
 
-	const double PI = 3.14159; //sets PI so that it cannot be altered later
-	double radius, circumference, area;
-
-	printf("\nPLEASE ENTENPUT RADIUS\n");
-	scanf_s("%lf", &radius);
-
-	circumference = 2 * PI * radius;
-	printf("the cicumference of the cirlce of that radius is %lf\n", circumference);
-
-	area = PI * radius * radius;
-	printf("the area of the cirlce of that radius is %lf\n", area);
-
-
-	return 0;
-}
+/*Hypotenuse of a triangle calculator*/
+//#include <math.h> //contains sqrt function
+//int main() {
+//
+//	double A, B, C;
+//
+//	printf("PLEASE ENTER INPUT A:\n");
+//	scanf_s("%lf", &A);
+//	printf("PLEASE ENTER AN INPUT B:\n");
+//	scanf_s("%lf", &B);
+//
+//	C = sqrt(A * A + B * B);
+//
+//	printf("THE HYPOTENUSE IS: %lf \n", C);
+//
+//	return 0;
+//}
