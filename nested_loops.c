e02ea58a2e14298bb1269b28639dcb10130a0d0e
Adam Davoren King <adamdavorenking21@gmail.com>
2024-12-31 16:19:01 +0000

    exploring nested loops

---
+++ nested_loops.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+int main(){
+
+// for some reason code in vid wouldn't work 
+//nested loops just basically work by having outside loop start then inside loop goes until it exits then outside loop advances and so forth
+
+	for (int i = 1; i <= rows; i++)
+	{
+		for (int j = 1; j <= columns; j++)
+		{
+			printf("%c", symbol);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+*/
+
