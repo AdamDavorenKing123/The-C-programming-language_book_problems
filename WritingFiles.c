875f6e21a6e12f973f83fb3370239f0d9a2d1ab6
Adam Davoren King <adamdavorenking21@gmail.com>
2024-12-31 18:55:37 +0000

    writng files

---
+++ WritingFiles.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//
+//int main() {
+//	/*FILE* pF = fopen("test.txt", "a"); // "w" is the mode for writing files
+//									   // "a" is the mode for appending into a file
+//	fprintf(pF, "\nSpongebob Squarepants");
+//
+//	*/
+//	/*Deleting a file*/
+//	if (remove("test.txt") == 0)
+//	{
+//		printf("That file was just deleted");
+//	}
+//	else {
+//		printf("that file was not deleted")
+//	}
+//	fclose(pF);  //good practice to close files at the end of program
+//		return 0;
+//}
